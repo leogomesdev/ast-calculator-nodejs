e2a8b783952c6386b4cbd1435249ebb25ff2a3ca
Leonardo Gomes <leogomesdev@gmail.com>
2020-11-30 01:17:19 -0300

    Improves test cases

--- tests/calculator/calculator.service.spec.ts
+++ tests/calculator/calculator.service.spec.ts
@@ -3,21 +3,22 @@ import CalculatorValidator from '../../src/calculator/calculator.validator';
 import OperatorsDictionary from '../../src/calculator/operators.dictionary';
 
 describe('CalculatorService', () => {
-  const operatorsList: string[] = [...(OperatorsDictionary.operatorsList.keys())];
+  const validMathOperators: string[] = new OperatorsDictionary().ALL;
+  
   let calculatorValidator: CalculatorValidator;
-  calculatorValidator = new CalculatorValidator(operatorsList);
+  calculatorValidator = new CalculatorValidator(validMathOperators);
 
   let calculatorService: CalculatorService;
-  calculatorService = new CalculatorService(OperatorsDictionary.operatorsList, calculatorValidator);
+  calculatorService = new CalculatorService(calculatorValidator);
 
   describe('calculate', () => {
     describe('single operations', () => {
       const cases: { operationName: string, testCase: string, expectedResult: number }[] = [
         { operationName: 'plus', testCase: '1 plus 1', expectedResult: 2 },
         { operationName: 'minus', testCase: '1000 minus 1', expectedResult: 999 },
-        { operationName: 'into', testCase: '3 into 4', expectedResult: 12 },
         { operationName: 'times', testCase: '4 times 4', expectedResult: 16 },
         { operationName: 'divided', testCase: '20 divided 5', expectedResult: 4 },
+        { operationName: '-x', testCase: '-10 times 50', expectedResult: -500 },
       ];
 
       cases.forEach((item) => {
@@ -32,9 +33,9 @@ describe('CalculatorService', () => {
       const cases: { operationName: string, testCase: string, expectedResult: number }[] = [
         { operationName: 'plus and minus', testCase: '1 plus (17 minus 4)', expectedResult: 14 },
         { operationName: 'minus and times', testCase: '1000 minus (1 times 100)', expectedResult: 900 },
-        { operationName: 'into and plus', testCase: '(3 into 4) plus 10', expectedResult: 22 },
         { operationName: 'times and divided', testCase: '(4 times 4) divided 2', expectedResult: 8 },
         { operationName: 'divided and plus', testCase: '(20 divided 5) plus 10', expectedResult: 14 },
+        { operationName: 'big', testCase: '(-9 plus 10) plus 5 times (9 minus (2 plus 1)) minus 5 divided 2', expectedResult: 18.5 },
       ];
 
       cases.forEach((item) => {
@@ -47,17 +48,22 @@ describe('CalculatorService', () => {
 
     it(`throws error for invalid operations`, () => {
       expect(() => calculatorService.calculate('1 times John'))
-        .toThrowError('Invalid arguments');
+        .toThrowError('The expression has a typo');
     });
 
     it(`throws error for operations with a typo`, () => {
       expect(() => calculatorService.calculate('1 time 1'))
-        .toThrow('Invalid arguments');
+        .toThrow('The expression has a typo');
     });
 
-    it(`throws error incomplete operations`, () => {
-      expect(() => calculatorService.calculate('1 times'))
-        .toThrow('Unexpected end of input');
+    it(`throws error for incomplete operations`, () => {
+      expect(() => calculatorService.calculate('(1 minus 2) times'))
+        .toThrow('Expect to find an operand');
+    });
+
+    it(`throws error for not closed parentheses`, () => {
+      expect(() => calculatorService.calculate('(1 minus 2 times 3'))
+        .toThrow('Expect to close parentheses');
     });
   });
 });
--- tests/calculator/calculator.validator.spec.ts
+++ tests/calculator/calculator.validator.spec.ts
@@ -2,16 +2,16 @@ import CalculatorValidator from '../../src/calculator/calculator.validator';
 import OperatorsDictionary from '../../src/calculator/operators.dictionary';
 
 describe('CalculatorValidator', () => {
-  const operatorsList: string[] = [...(OperatorsDictionary.operatorsList.keys())];
+  const validMathOperators: string[] = new OperatorsDictionary().ALL;
+  
   let calculatorValidator: CalculatorValidator;
-  calculatorValidator = new CalculatorValidator(operatorsList);
+  calculatorValidator = new CalculatorValidator(validMathOperators);
 
   describe('isValid', () => {
     describe('single operations', () => {
       const cases: Map<string, string> = new Map<string, string>([
         ['plus', '1 plus 1'],
         ['minus', '1000 minus 1'],
-        ['into', '3 into 4'],
         ['times', '4 times 4'],
         ['divided', '20 divided 5'],
       ]);
@@ -28,7 +28,6 @@ describe('CalculatorValidator', () => {
       const cases: Map<string, string> = new Map<string, string>([
         ['plus and minus', '1 plus (17 minus 4)'],
         ['minus and times', '1000 minus (1 times 100)'],
-        ['into and plus', '(3 into 4) plus 10'],
         ['times and divided', '(4 times 4) divided 2'],
         ['divided and plus', '(20 divided 5) plus 10'],
       ]);
